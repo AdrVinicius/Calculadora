c0068d7bf2eae66bd60b88bfbfe444429d2228ab
Adrian Vinicius <115122220+AdrVinicius@users.noreply.github.com>
2022-12-15 22:47:52 -0300

    Update Profissional.java

--- Profissional.java
+++ Profissional.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Profissional {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        calculadora.Nome = "Adrian";
+
+
+        System.out.println("Digite seu nome");
+        Scanner sc = new Scanner(System.in);
+        calculadora.Nome = sc.nextLine();
+        
+        
         calculadora.SalarioMensal = 2300.00;
 
         System.out.println("Nome: " + calculadora.Nome);
@@ -16,12 +24,14 @@ public class Profissional {
         tela1.tipoItem = "Login";
         tela1.complexidade = 3;
         listaItenSistemas[0] = tela1;
+        System.out.println(" " );
 
         ItemSistema tela2 = new ItemSistema();
         tela2.nomeItem = "Tela de Cadastro";
         tela2.tipoItem = "Tela Normal";
         tela2.complexidade = 3;
         listaItenSistemas[1] = tela2;
+        
 
         ItemSistema tela3 = new ItemSistema();
         tela3.nomeItem = "Tela de Pesquisa";
@@ -46,10 +56,78 @@ public class Profissional {
             System.out.println("Complexidade do Item: " + itemSistema.complexidade);
             System.out.println("Quantidade de Horas Previstas: " + itemSistema.CalcularQuantidadeHoras());
             numeroTela ++;
+            System.out.println(" ");
         }
 
         double valorTotalSistema = calculadora.CalcularValorHora() * totalHorasTelas;
 
         System.out.println("Valor Total do Orçamento: " + valorTotalSistema);
+        System.out.println("----------------------------------------------------------------------------------------------------------------------" );
+
+
+
+        System.out.println("Você deseja o item 1? Digite S para (sim) ou digite N para (não) ");
+        String digitadoPrimeiro = sc.nextLine();
+
+        if (digitadoPrimeiro.equals("s")){
+            System.out.println("Item 1 selecionado.");
+            //int valorPrimeiro = 24;
+
+        }
+
+        int valorPrimeiro = 72;
+
+
+        System.out.println("Você deseja o item 2? Digite S para (sim) ou digite N para (não) ");
+        String digitadoSegundo = sc.nextLine();
+
+        if (digitadoSegundo.equals("s")){
+            System.out.println("Item 2 selecionado.");
+            //int valorSegundo = 48;
+
+        }
+        
+        int valorSegundo = 72;
+
+
+        System.out.println("Você deseja o item 3? Digite S para (sim) ou digite N para (não) ");
+        String digitadoTerceiro = sc.nextLine();
+
+        if (digitadoTerceiro.equals("s")){
+            System.out.println("Item 3 selecionado.");
+            //int valorTerceiro = 72;
+
+        }
+
+        int valorTerceiro = 48;
+
+
+
+        String digitadoQuarto = sc.nextLine();
+        if (digitadoQuarto.equals("s")){
+            System.out.println("Item 4 selecionado.");
+            //int valorTerceiro = 72;
+
+        }
+
+        int valorquarto = 24;
+
+
+
+        if(digitadoPrimeiro.equals("s") && digitadoSegundo.equals("s") && digitadoTerceiro.equals("s") && digitadoQuarto.equals("s")){
+            int resultaFinalHoras = valorPrimeiro + valorSegundo + valorTerceiro + valorquarto;
+            // 1 = 24 horas
+            // 2 = 48 horas
+            // 3 = 72 horas
+            System.out.println(" ");
+            System.out.println("A quantidade de horas é " + resultaFinalHoras + " e o valor em reais é " + 32.86 *resultaFinalHoras);
+            System.out.println("Com o acrescimo da taixa de gestão, o valor final fica em " + (32.85 *resultaFinalHoras) *1.32 );
+
+        }
+
+        sc.close();
     }
+
+ 
+    
 }
